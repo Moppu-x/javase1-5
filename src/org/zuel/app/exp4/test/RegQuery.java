dcf2ec87aa5ce686eab064a76d0448aedfc3c888
Moppu-x <xg924830930@gmail.com>
2020-11-08 10:34:18 +0800

    commit

--- src/org/zuel/app/exp4/test/RegQuery.java
+++ src/org/zuel/app/exp4/test/RegQuery.java
@@ -1,8 +1,23 @@
 package org.zuel.app.exp4.test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.zuel.app.exp4.dao.RegRecordDao;
+import org.zuel.app.exp4.model.RegRecord;
+
 public class RegQuery {
     public static void main(String[] args) {
         //查询挂号记录并展示;
-        
+        //rList用于存放查询结果;
+        List<RegRecord> rList = new ArrayList<>();
+        //使用Dao查询挂号记录数据;
+        RegRecordDao rDao = new RegRecordDao();
+        rList = rDao.getRegRecord();
+        Iterator<RegRecord> iter = rList.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next().toString());
+        }
     }
 }
